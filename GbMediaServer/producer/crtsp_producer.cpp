531c2742477c9bf246a4fceaef7da9ab7bfddce7
chensong <15850774503@163.com>
2025-11-18 19:07:38 +0800

    [增加rtmp和rtsp的协议]

---
+++ GbMediaServer/producer/crtsp_producer.cpp
@@ -0,0 +1,179 @@
+﻿/******************************************************************************
+ *  Copyright (c) 2025 The CRTC project authors . All Rights Reserved.
+ *
+ *  Please visit https://chensongpoixs.github.io for detail
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ ******************************************************************************/
+ /*****************************************************************************
+				   Author: chensong
+				   date:  2025-10-18
+ 输赢不重要，答案对你们有什么意义才重要。
+
+ 光阴者，百代之过客也，唯有奋力奔跑，方能生风起时，是时代造英雄，英雄存在于时代。或许世人道你轻狂，可你本就年少啊。 看护好，自己的理想和激情。
+
+
+ 我可能会遇到很多的人，听他们讲好2多的故事，我来写成故事或编成歌，用我学来的各种乐器演奏它。
+ 然后还可能在一个国家遇到一个心仪我的姑娘，她可能会被我帅气的外表捕获，又会被我深邃的内涵吸引，在某个下雨的夜晚，她会全身淋透然后要在我狭小的住处换身上的湿衣服。
+ 3小时候后她告诉我她其实是这个国家的公主，她愿意向父皇求婚。我不得已告诉她我是穿越而来的男主角，我始终要回到自己的世界。
+ 然后我的身影慢慢消失，我看到她眼里的泪水，心里却没有任何痛苦，我才知道，原来我的心被丢掉了，我游历全世界的原因，就是要找回自己的本心。
+ 于是我开始有意寻找各种各样失去心的人，我变成一块砖头，一颗树，一滴水，一朵白云，去听大家为什么会失去自己的本心。
+ 我发现，刚出生的宝宝，本心还在，慢慢的，他们的本心就会消失，收到了各种黑暗之光的侵蚀。
+ 从一次争论，到嫉妒和悲愤，还有委屈和痛苦，我看到一只只无形的手，把他们的本心扯碎，蒙蔽，偷走，再也回不到主人都身边。
+ 我叫他本心猎手。他可能是和宇宙同在的级别 但是我并不害怕，我仔细回忆自己平淡的一生 寻找本心猎手的痕迹。
+ 沿着自己的回忆，一个个的场景忽闪而过，最后发现，我的本心，在我写代码的时候，会回来。
+ 安静，淡然，代码就是我的一切，写代码就是我本心回归的最好方式，我还没找到本心猎手，但我相信，顺着这个线索，我一定能顺藤摸瓜，把他揪出来。
+
+
+ ******************************************************************************/
+ 
+#include "libmedia_transfer_protocol/rtp_rtcp/byte_io.h"
+#include "gb_media_server_log.h"
+#include "libmedia_transfer_protocol/rtp_rtcp/rtp_util.h"
+#include "libmedia_transfer_protocol/rtp_rtcp/rtp_packet_received.h"
+#include "libmedia_transfer_protocol/rtp_rtcp/rtcp_packet/common_header.h"
+
+#include "server/stream.h"
+#include "producer/rtc_producer.h"
+#include "server/session.h"
+#include "server/gb_media_service.h"
+#include "server/rtc_service.h"
+#include "libmedia_transfer_protocol/rtp_rtcp/video_rtp_depacketizer_h264.h"
+#include "utils/yaml_config.h"
+#include "libmedia_transfer_protocol/rtp_rtcp/rtcp_packet/compound_packet.h"
+#include "libmedia_transfer_protocol/rtp_rtcp/rtcp_packet/receiver_report.h"
+#include "libmedia_transfer_protocol/rtp_rtcp/rtcp_packet/psfb.h"
+#include "libmedia_transfer_protocol/rtp_rtcp/rtcp_packet/sender_report.h"
+#include "libmedia_transfer_protocol/rtp_rtcp/rtcp_packet/sdes.h"
+#include "libmedia_transfer_protocol/rtp_rtcp/rtcp_packet/extended_reports.h"
+#include "libmedia_transfer_protocol/rtp_rtcp/rtcp_packet/bye.h"
+#include "libmedia_transfer_protocol/rtp_rtcp/rtcp_packet/rtpfb.h"
+#include "libmedia_transfer_protocol/rtp_rtcp/rtcp_packet/pli.h"
+#include "libmedia_transfer_protocol/rtp_rtcp/rtcp_packet/compound_packet.h"
+#include "producer/crtsp_producer.h"
+namespace gb_media_server {
+
+
+ 
+	RtspProducer::RtspProducer(
+		const std::shared_ptr<Stream> & stream, 
+		const std::shared_ptr<Session> &s)
+		:  Producer(  stream, s) 
+		 
+	{
+		//local_ufrag_ = GetUFrag(8);
+		//local_passwd_ = GetUFrag(32);
+		///uint32_t audio_ssrc = GetSsrc(10);
+		//uint32_t video_ssrc = audio_ssrc + 1;
+
+		//sdp_.SetLocalUFrag(local_ufrag_);
+		//sdp_.SetLocalPasswd(local_passwd_);
+		//sdp_.SetAudioSsrc(audio_ssrc);
+		//sdp_.SetVideoSsrc(video_ssrc);
+		//dtls_certs_.Init();
+		//dtls_.Init();
+		//dtls_.SignalDtlsSendPakcet.connect(this, &PlayRtcUser::OnDtlsSendPakcet);
+		//dtls_.SignalDtlsHandshakeDone.connect(this, &PlayRtcUser::OnDtlsHandshakeDone);
+		//dtls_.SignalDtlsClose.connect(this, &PlayRtcUser::OnDtlsClosed);
+
+
+	 
+
+
+		
+	
+	}
+	RtspProducer::~RtspProducer()
+	{
+		GBMEDIASERVER_LOG_T_F(LS_INFO);
+
+		//dtls_.SignalDtlsSendPakcet.disconnect(this);
+		//dtls_.SignalDtlsHandshakeDone.disconnect(this);
+		//dtls_.SignalDtlsClose.disconnect(this);
+
+		 
+	}
+	void RtspProducer::OnRecv(const rtc::CopyOnWriteBuffer&  buffer1)
+	{
+#if 0
+		memcpy(recv_buffer_ + recv_buffer_size_, buffer1.data(), buffer1.size());
+		recv_buffer_size_ += buffer1.size();
+		//recv_buffer_.SetData(buffer1);
+		int32_t   parse_size = 0;
+
+		 
+		while (recv_buffer_size_ - parse_size > 2)
+		{ 
+			int16_t  payload_size = libmedia_transfer_protocol::ByteReader<int16_t>::ReadBigEndian((&recv_buffer_[parse_size]));
+			 
+			if ((recv_buffer_size_ - parse_size) < (payload_size + 2))
+			{
+				// 当不不够一个完整包需要继续等待下一个包的到来
+				//GBMEDIASERVER_LOG(LS_INFO) << "tcp tail small !!!  (read_bytes -parse_size:" << (recv_buffer_size_ - parse_size) << ") payload_size:" << payload_size;
+				break;
+			}
+			parse_size += 2;  
+			if (libmedia_transfer_protocol::IsRtpPacket(rtc::ArrayView<uint8_t>(recv_buffer_ + parse_size, payload_size)))
+			{
+				libmedia_transfer_protocol::RtpPacketReceived  rtp_packet_received; 
+				bool ret = rtp_packet_received.Parse(recv_buffer_  + parse_size, payload_size);
+				if (!ret)
+				{ 
+					GBMEDIASERVER_LOG(LS_WARNING) << "rtp parse failed !!! size:" << (recv_buffer_size_ - parse_size); //<< "  , hex :" << rtc::hex_encode((const char *)(buffer.begin() + paser_size), (size_t)(read_bytes - paser_size));
+				}
+				else
+				{
+					//RTC_LOG(LS_INFO) << "rtp info :" << rtp_packet_received.ToString();
+					if (rtp_packet_received.PayloadType() == 96)
+					{
+						//mpeg_decoder_->parse( rtp_packet_received.payload().data(), rtp_packet_received.payload_size());; 
+					} 
+				}
+			}
+			else if (libmedia_transfer_protocol::IsRtcpPacket(rtc::ArrayView<uint8_t>(recv_buffer_ + parse_size, payload_size/*read_bytes - paser_size*/)))
+			{
+				libmedia_transfer_protocol::rtcp::CommonHeader rtcp_block;  //rtcp_packet;
+				bool ret = rtcp_block.Parse(recv_buffer_ + parse_size, payload_size/* read_bytes - paser_size*/);
+				if (!ret)
+				{
+					GBMEDIASERVER_LOG(LS_WARNING) << "rtcp parse failed !!!";
+				}
+				//else
+				{
+					//parse_size += payload_size;
+					//	RTC_LOG(LS_INFO) << "rtcp info :" << rtcp_block.ToString();
+				}
+			}
+			else
+			{
+				GBMEDIASERVER_LOG(LS_ERROR) << " not know type --> : payload_size: " << payload_size;
+				//parse_size += payload_size;
+			}
+			parse_size += payload_size;
+ 
+		}
+		//GBMEDIASERVER_LOG(LS_INFO) << "read_bytes:" << recv_buffer_size_ << ", parse_size:" << parse_size;
+		if (recv_buffer_size_ - parse_size > 0)
+		{
+			//memcpy((char *)recv_buffer_.begin(), buffer.data() + parse_size, (buffer.size() - parse_size));
+			recv_buffer_size_ -= parse_size;;
+			memmove(recv_buffer_, recv_buffer_+ parse_size,recv_buffer_size_);
+			 
+		}
+		else
+		{
+			recv_buffer_size_ = 0;
+			parse_size = 0;
+			//memcpy((char *)recv_buffer_.begin(), buffer.begin() + parse_size, (read_bytes - parse_size));
+			//recv_buffer_size_ = read_bytes - parse_size;
+		}
+		
+#endif // recv_buffer_size_
+	}
+}
+
+  
